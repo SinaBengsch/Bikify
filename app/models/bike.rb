8a850cbd79e5686bd7d59c2ba9989595a2c89fd5
Sina Bengsch <sinabengsch@gmail.com>
2020-05-27 18:46:10 +0200

    added validations in bike and booking model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,9 @@
 class Bike < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :location, presence: true
+  validates :price_per_day, presence: true, numericality: { greater_than: 0 }
 end
