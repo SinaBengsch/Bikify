db5b1b4d1fdc87a7e01d4ab9170af23c90469519
yue yin <yinyue626@gmail.com>
2020-05-29 14:54:24 +0200

    fixed-dashboard-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_125152) do
+ActiveRecord::Schema.define(version: 2020_05_29_123555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_125152) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
@@ -58,6 +59,14 @@ ActiveRecord::Schema.define(version: 2020_05_28_125152) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["bike_id"], name: "index_bookings_on_bike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "bike_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bike_id"], name: "index_reviews_on_bike_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,4 +86,5 @@ ActiveRecord::Schema.define(version: 2020_05_28_125152) do
   add_foreign_key "bikes", "users"
   add_foreign_key "bookings", "bikes"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bikes"
 end
